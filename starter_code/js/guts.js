6ac9a22f2b0f9d05a6afa2f5ecfd821b7d67f143
trevonwilliams227 <trevonwilliams227@gmail.com>
2016-10-20 22:20:20 +0000

    remove read

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -2,3 +2,37 @@
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
+ function add(a, b){
+     return a + b;
+ }
+
+function multiply(a, b){
+    return a * b;
+}  
+
+function subtract(a, b){
+    return a - b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function power(a, b){
+    return a ^ b;
+}
+
+function square_root(a){
+    return Math.sqrt(a);
+}
+
+function logBase10(a){
+    return  Math.log10(a)
+}
+
+function generateRandomNumber(){
+    return Math.floor((Math.random() * 100) + 1);
+    
+}
+
+    // body...
